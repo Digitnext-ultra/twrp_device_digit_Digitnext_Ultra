d7a5df031f96a71685b7105becba2eae2a5093f6
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2026-02-13 00:23:12 +0500

    omni > twrp
    renamed twrp from omni

--- twrp_Digitnext_Ultra.mk
+++ twrp_Digitnext_Ultra.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Digitnext_Ultra device
 $(call inherit-product, device/digit/Digitnext_Ultra/device.mk)
 
 PRODUCT_DEVICE := Digitnext_Ultra
-PRODUCT_NAME := omni_Digitnext_Ultra
+PRODUCT_NAME := twrp_Digitnext_Ultra
 PRODUCT_BRAND := Digit
 PRODUCT_MODEL := Digitnext_Ultra
 PRODUCT_MANUFACTURER := digit
